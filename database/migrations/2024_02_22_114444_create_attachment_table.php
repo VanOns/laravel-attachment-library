a314e6d967023f794e53bfae86e072883b8900c0
Dangelo Brusorio <ddangelo.brusorio@gmail.com>
2024-04-30 16:09:23 +0200

    fix: change string to integer for attachment size

--- database/migrations/2024_02_22_114444_create_attachment_table.php
+++ database/migrations/2024_02_22_114444_create_attachment_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('disk');
             $table->string('mime_type');
             $table->string('path')->nullable();
-            $table->string('size');
+            $table->integer('size');
             $table->timestamps();
         });
     }
