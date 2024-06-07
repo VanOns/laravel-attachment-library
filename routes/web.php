0869ca8af0835f02e42acb6128ff433fa0ac3620
Dangelo Brusorio <ddangelo.brusorio@gmail.com>
2024-06-07 10:47:18 +0200

    refactor: move GlideController to Glide directory

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use VanOns\LaravelAttachmentLibrary\Glide\GlideController;
 use VanOns\LaravelAttachmentLibrary\Http\Controllers\AttachmentController;
 
 Route::get('files/{attachment}', AttachmentController::class)
     ->where('attachment', '.*')
     ->middleware(['web'])
     ->name('attachment');
-
-Route::get('img/{path}', GlideController::class)
-    ->where('path', '.*')
-    ->name('glide');
