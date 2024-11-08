8ab1a0f2eeef4b8f6ce9f68bbf288907f8438f83
Danique Wijnalda <danique@van-ons.nl>
2024-11-08 14:33:20 +0100

    fix: add null check for user

--- src/Observers/AttachmentObserver.php
+++ src/Observers/AttachmentObserver.php
@@ -8,12 +8,12 @@ class AttachmentObserver
 {
     public function creating(Attachment $attachment): void
     {
-        $attachment->created_by = auth()->user()->getAuthIdentifier();
-        $attachment->updated_by = auth()->user()->getAuthIdentifier();
+        $attachment->created_by = auth()->user()?->getAuthIdentifier();
+        $attachment->updated_by = auth()->user()?->getAuthIdentifier();
     }
 
     public function updating(Attachment $attachment): void
     {
-        $attachment->updated_by = auth()->user()->getAuthIdentifier();
+        $attachment->updated_by = auth()->user()?->getAuthIdentifier();
     }
 }
