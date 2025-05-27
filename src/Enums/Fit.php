728e8a02ec90297f8bc8ca2e2028b69d34a4dc64
Danique Wijnalda <danique@van-ons.nl>
2025-05-27 17:40:25 +0200

    feat: change enum value and order

--- src/Enums/Fit.php
+++ src/Enums/Fit.php
@@ -5,9 +5,9 @@ namespace VanOns\LaravelAttachmentLibrary\Enums;
 enum Fit: string
 {
     case CONTAIN = 'contain';
-    case MAX = 'max';
-    case FILL = 'fill';
-    case FILLMAX = 'fill-max';
-    case STRETCH = 'stretch';
     case CROP = 'crop';
+    case FILL = 'fill';
+    case FILL_MAX = 'fill-max';
+    case MAX = 'max';
+    case STRETCH = 'stretch';
 }
