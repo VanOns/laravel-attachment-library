bc7e847937228996ca57d6d351283418a15dd83a
Jim Elvers <jim@van-ons.nl>
2026-06-11 11:43:08 +0200

    feat: serve file through storage facade in `AttachmentController`

--- src/Http/Controllers/AttachmentController.php
+++ src/Http/Controllers/AttachmentController.php
@@ -4,6 +4,7 @@ namespace VanOns\LaravelAttachmentLibrary\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Support\Facades\Storage;
 use Symfony\Component\HttpFoundation\Response;
 use VanOns\LaravelAttachmentLibrary\Http\Middleware\EnsureRenderableAttachment;
 use VanOns\LaravelAttachmentLibrary\Models\Attachment;
@@ -12,7 +13,9 @@ class AttachmentController implements HasMiddleware
 {
     public function __invoke(Request $request, Attachment $attachment): Response
     {
-        return response()->file($attachment->absolute_path);
+        return response(Storage::disk($attachment->disk)->get($attachment->full_path), headers: [
+            'Content-Type' => $attachment->mime_type,
+        ]);
     }
 
     public static function middleware(): array
