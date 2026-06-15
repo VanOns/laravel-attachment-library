f9c1e4ce4db1bedf0aa02f2f2ed9abd32107dbcd
Danique Wijnalda <danique@van-ons.nl>
2026-06-15 14:50:47 +0200

    style: sort php-cs-fixer rules

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -17,14 +17,14 @@ return (new PhpCsFixer\Config())
     ->setParallelConfig(ParallelConfigFactory::detect())
     ->setRules([
         '@PSR12' => true,
-        'array_syntax' => true,
         'array_indentation' => true,
+        'array_syntax' => true,
         'explicit_indirect_variable' => true,
         'explicit_string_variable' => true,
         'method_chaining_indentation' => true,
         'no_unused_imports' => true,
+        'phpdoc_line_span' => true,
         'single_quote' => true,
         'trailing_comma_in_multiline' => true,
-        'phpdoc_line_span' => true,
     ])
     ->setFinder($finder);
